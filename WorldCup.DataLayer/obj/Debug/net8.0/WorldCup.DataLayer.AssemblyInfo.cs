079448baa2dcdfa1581bc7d992b4e382c933df98
j3rk0vic <ijerkovic13@gmail.com>
2025-05-24 16:06:06 +0200

    committing first wpf files

--- WorldCup.DataLayer/obj/Debug/net8.0/WorldCup.DataLayer.AssemblyInfo.cs
+++ WorldCup.DataLayer/obj/Debug/net8.0/WorldCup.DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldCup.DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d67db458118b68fe15f959249054df85c78e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldCup.DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldCup.DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WorldCup.WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorldCup.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContinue_Click(object sender, RoutedEventArgs e)
+        {
+            string championship = rbMen.IsChecked == true ? "men" : "women";
+            string language = rbCroatian.IsChecked == true ? "hr" : "en";
+            string? resolution = (cbResolution.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            string settings = $"{championship}, {language}, {resolution}";
+            File.WriteAllText("settings.txt", settings);
+
+            MessageBox.Show("Settings saved!");
+        }
+    }
+}
